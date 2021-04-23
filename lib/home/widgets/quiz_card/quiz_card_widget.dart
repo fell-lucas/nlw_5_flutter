0ab5200d316d11373a1f843ef2d1bcc93eeec6a9
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-04-23 13:53:48 -0300

    level button, quiz card

---
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:nlw_5_flutter/core/app_colors.dart';
+import 'package:nlw_5_flutter/core/core.dart';
+
+class QuizCardWidget extends StatelessWidget {
+  const QuizCardWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        border: Border.fromBorderSide(BorderSide(color: AppColors.border)),
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 40,
+            width: 40,
+            child: Image.asset(AppImages.blocks),
+          ),
+          SizedBox(height: 20),
+          Text("Gerenciamento de Estado", style: AppTextStyles.heading15),
+          SizedBox(height: 20),
+          Row(children: [
+            Expanded(
+              flex: 1,
+              child: Text("3/10", style: AppTextStyles.body11),
+            ),
+            Expanded(
+              flex: 4,
+              child: LinearProgressIndicator(
+                value: 0.3,
+                backgroundColor: AppColors.chartSecondary,
+                valueColor: AlwaysStoppedAnimation<Color>(
+                  AppColors.chartPrimary,
+                ),
+              ),
+            ),
+          ])
+        ],
+      ),
+    );
+  }
+}
